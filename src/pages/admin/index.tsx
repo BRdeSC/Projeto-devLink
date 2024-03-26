dfef57b1bac0ff3ee022ead13e7392c249d1a4a5
Bruno Castro <brunodsc.trabalho@gmail.com>
2024-03-26 00:22:12 -0300

    Cadastrando no banco de dados

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,8 +1,18 @@
-import { useState } from "react"; 
+import { FormEvent, useState } from "react"; 
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 
 import { FiTrash } from "react-icons/fi";
+import { db } from '../../services/firebaseConnection'
+import {
+    addDoc,
+    collection,
+    onSnapshot,
+    query,
+    orderBy,
+    doc,
+    deleteDoc,
+} from 'firebase/firestore'
 
 export function Admin(){
     const [nameInput, setNameInput] = useState("")
@@ -10,11 +20,38 @@ export function Admin(){
     const [textColorInput, setTextColorInput] = useState("#f1f1f1")
     const [backgroundColorInput, setBackgroundInput] = useState("#121212")
 
+    function handleRegister(e: FormEvent){
+        e.preventDefault();
+        
+        if(nameInput === "" || urlInput === ""){
+            alert("Preencha todos os campos")
+            return;
+        }
+
+        addDoc(collection(db, "links"), {
+            name: nameInput,
+            url: urlInput,
+            bg: backgroundColorInput,
+            color: textColorInput,
+            created: new Date()
+        })
+        .then(() => {
+            setNameInput("")
+            setUrlInput("")
+            console.log("CADASTRADO COM SUCESSO!")
+        })
+        .catch((error) => {
+            console.log("ERRO AO CADASTRAR NO BANCO" + error)
+        })
+    
+        
+    }
+
     return(
         <div className="flex items-center flex-col min-h-screen pb-7 px-2">
             <Header/>
 
-            <form className="flex flex-col mt-8 mb-3 w-full max-w-xl">
+            <form className="flex flex-col mt-8 mb-3 w-full max-w-xl" onSubmit={handleRegister}>
                 <label className="text-white font-medium mt-2 mb-2">Nome do Link</label>
                 <Input
                     placeholder="Digite o nome do link..."
@@ -73,7 +110,7 @@ export function Admin(){
             </h2>
 
             <article
-                className="flex items-center justify-between w-11/12 max-w-xl rounded py-3 mb-2 select-none"
+                className="flex items-center justify-between w-11/12 max-w-xl rounded py-3 px-2 mb-2 select-none"
                 style={{ backgroundColor: "#2563EB", color: "#FFF" }}
             >
                 <p>Canal do youtube</p>
